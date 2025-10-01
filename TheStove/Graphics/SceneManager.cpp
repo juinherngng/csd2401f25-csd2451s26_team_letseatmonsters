300201be73d99ae92157a90fa02f5155155c5080
y.chunwee <yatchunwee55@gmail.com>
2025-10-01 17:15:48 +0800

    updated centralized sprites

--- TheStove/Graphics/SceneManager.cpp
+++ TheStove/Graphics/SceneManager.cpp
@@ -137,7 +137,7 @@ void Scene::LoadTest() {
 	// Set background image
 	SetSceneBackground("../assets/Background.png");
 
-	GameObject* player = SpawnSprite("../assets/mc_front.png",
+	GameObject* player = SpawnSprite("../assets/mc_sprite_front.png",
 		glm::vec3(400, 400, 0), //Position
 		glm::vec2(128, 128));   //Scale
 
@@ -151,7 +151,7 @@ void Scene::LoadTest() {
 
 		player->SetScale({ 96.0f, 96.0f, 1.0f });     // visual size
 		player->SetColliderSize({ 64.0f, 128.0f });    // tight hitbox
-		player->SetColliderOffset({ -32.0f, 0.0f });    // nudge down slightly
+		player->SetColliderOffset({ 0.0f, 0.0f });    // nudge down slightly
 
 	}
 	else {
@@ -212,19 +212,19 @@ void Scene::Update(float deltaTime, GLFWwindow* window) {
 	// Keyboard movement intent (WASD) + facing texture swap
 	glm::vec2 desiredMove{ 0.0f, 0.0f };
 	if (inputManager.IsKeyPressed(GLFW_KEY_W)) {
-		sprite->SetTexture(ResourceManager::Instance().LoadTexture("mc_back", "../assets/mc_back.png"));
+		sprite->SetTexture(ResourceManager::Instance().LoadTexture("mc_back", "../assets/mc_sprite_back.png"));
 		desiredMove.y -= moveSpeed; // up
 	}
 	if (inputManager.IsKeyPressed(GLFW_KEY_S)) {
-		sprite->SetTexture(ResourceManager::Instance().LoadTexture("mc_front", "../assets/mc_front.png"));
+		sprite->SetTexture(ResourceManager::Instance().LoadTexture("mc_front", "../assets/mc_sprite_front.png"));
 		desiredMove.y += moveSpeed; // down
 	}
 	if (inputManager.IsKeyPressed(GLFW_KEY_A)) {
-		sprite->SetTexture(ResourceManager::Instance().LoadTexture("mc_sideleft", "../assets/mc_sideleft.png"));
+		sprite->SetTexture(ResourceManager::Instance().LoadTexture("mc_sideleft", "../assets/mc_sprite_left.png"));
 		desiredMove.x -= moveSpeed; // left
 	}
 	if (inputManager.IsKeyPressed(GLFW_KEY_D)) {
-		sprite->SetTexture(ResourceManager::Instance().LoadTexture("mc_sideright", "../assets/mc_sideright.png"));
+		sprite->SetTexture(ResourceManager::Instance().LoadTexture("mc_sideright", "../assets/mc_sprite_right.png"));
 		desiredMove.x += moveSpeed; // right
 	}
 
