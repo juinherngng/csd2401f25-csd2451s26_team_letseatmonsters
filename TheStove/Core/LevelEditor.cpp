07b977e52e9a26d0d17738c5c98136065db583d3
juinherngng <juinherng.ng@digipen.edu>
2025-11-04 18:06:59 +0800

    fixed assertion for viewport size
    code only checked for Y value. added another check for X value

--- TheStove/Core/LevelEditor.cpp
+++ TheStove/Core/LevelEditor.cpp
@@ -736,9 +736,13 @@ void LevelEditor::DrawUI(Scene& scene) {
 		if (viewportSize.y < 64.f) {
 			viewportSize.y = 64.f;
 		}
+		if (viewportSize.x < 64.f) {
+			viewportSize.x = 64.f;
+		}
 
 		// One big interactive area
 		ImGui::InvisibleButton("##SceneViewport", viewportSize, ImGuiButtonFlags_MouseButtonLeft);
+
 		const bool viewportHovered = ImGui::IsItemHovered();
 		const bool viewportActive = ImGui::IsItemActive();
 		InputManager::Get().SetSceneViewportWantsGameMouse(viewportHovered || viewportActive);
