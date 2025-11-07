31226e31ae18c902a7a98239aaeba7fd121880a1
juinherngng <juinherng.ng@digipen.edu>
2025-11-07 11:40:22 +0800

    fixed more warnings

--- TheStove/Core/DebugUI.hpp
+++ TheStove/Core/DebugUI.hpp
@@ -29,8 +29,8 @@ class Scene;
 struct SystemPerformance
 {
 	std::string name;				// Name of the system
-	float percentageOf;				// The %tage of the total system time (relative distribution)
-	float percentageOfFrame;		// The %tage of the frame time (absolute usage)
+	float percentageOf = 0.0f;		// The %tage of the total system time (relative distribution)
+	float percentageOfFrame = 0.0f;	// The %tage of the frame time (absolute usage)
 	float peakPercentage = 0.0f;	// Peak percentage recorded
 	float avgPercentage = 0.0f;		// Average percentage
 	int sampleCount = 0;			// Number of samples for averaging
