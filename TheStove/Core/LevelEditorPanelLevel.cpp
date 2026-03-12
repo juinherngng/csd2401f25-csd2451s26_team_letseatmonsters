587885959088685493d9785975b6e055b9ffbacb
chuuniby <takanashirikka501@gmail.com>
2026-03-12 13:33:08 +0800

    Fix merge

--- TheStove/Core/LevelEditorPanelLevel.cpp
+++ TheStove/Core/LevelEditorPanelLevel.cpp
@@ -1423,62 +1423,62 @@ namespace LEPANELLEVEL {
 					scene.RebuildColliders();
 				}
 
-			if (colliderEnabled) {
-				float avail = ImGui::GetContentRegionAvail().x;
-				float gap = ImGui::GetStyle().ItemInnerSpacing.x;
-				float fieldW = (avail - gap) * 0.5f;
+				if (colliderEnabled) {
+					float avail = ImGui::GetContentRegionAvail().x;
+					float gap = ImGui::GetStyle().ItemInnerSpacing.x;
+					float fieldW = (avail - gap) * 0.5f;
 
-				// Size
-				ImGui::TextUnformatted("Size");
-				// row of 2 fields (x, y)
-				ImGui::PushItemWidth(fieldW);
-				bool sizeChangedX = ImGui::DragFloat("x##col_size_x", &colliderSize.x, 1.0f, 0.0f, 99999.0f);
-				bool sizeActivatedX = ImGui::IsItemActivated();
-				ImGui::SameLine(0.0f, gap);
-				bool sizeChangedY = ImGui::DragFloat("y##col_size_y", &colliderSize.y, 1.0f, 0.0f, 99999.0f);
-				bool sizeActivatedY = ImGui::IsItemActivated();
-				ImGui::PopItemWidth();
+					// Size
+					ImGui::TextUnformatted("Size");
+					// row of 2 fields (x, y)
+					ImGui::PushItemWidth(fieldW);
+					bool sizeChangedX = ImGui::DragFloat("x##col_size_x", &colliderSize.x, 1.0f, 0.0f, 99999.0f);
+					bool sizeActivatedX = ImGui::IsItemActivated();
+					ImGui::SameLine(0.0f, gap);
+					bool sizeChangedY = ImGui::DragFloat("y##col_size_y", &colliderSize.y, 1.0f, 0.0f, 99999.0f);
+					bool sizeActivatedY = ImGui::IsItemActivated();
+					ImGui::PopItemWidth();
 
-				if (sizeActivatedX || sizeActivatedY) {
-					PushUndoSnapshot(editor, scene);
+					if (sizeActivatedX || sizeActivatedY) {
+						PushUndoSnapshot(editor, scene);
+					}
+
+					if (sizeChangedX || sizeChangedY) {
+						obj->SetColliderSize({ colliderSize.x, colliderSize.y });
+						scene.RebuildColliders();
+						SyncColliderDefaults({ colliderSize.x, colliderSize.y }, colliderOff);
+					}
+
+					ImGui::Spacing();
+
+					// Offset
+					ImGui::TextUnformatted("Offset");
+					ImGui::PushItemWidth(fieldW);
+					bool offChangedX = ImGui::DragFloat("x##col_off_x", &colliderOff.x, 1.0f, -99999.0f, 99999.0f);
+					bool offActivatedX = ImGui::IsItemActivated();
+					ImGui::SameLine(0.0f, gap);
+					bool offChangedY = ImGui::DragFloat("y##col_off_y", &colliderOff.y, 1.0f, -99999.0f, 99999.0f);
+					bool offActivatedY = ImGui::IsItemActivated();
+					ImGui::PopItemWidth();
+
+					if (offActivatedX || offActivatedY) {
+						PushUndoSnapshot(editor, scene);
+					}
+
+					if (offChangedX || offChangedY) {
+						obj->SetColliderOffset({ colliderOff.x, colliderOff.y });
+						SyncColliderDefaults(colliderSize, { colliderOff.x, colliderOff.y });
+						scene.RebuildColliders();
+					}
+				}
+				else {
+					ImGui::TextDisabled("Collider disabled");
 				}
 
-				if (sizeChangedX || sizeChangedY) {
-					obj->SetColliderSize({ colliderSize.x, colliderSize.y });
-					scene.RebuildColliders();
-					SyncColliderDefaults({ colliderSize.x, colliderSize.y }, colliderOff);
-				}
+				ImGui::NextColumn();
 
-				ImGui::Spacing();
-
-				// Offset
-				ImGui::TextUnformatted("Offset");
-				ImGui::PushItemWidth(fieldW);
-				bool offChangedX = ImGui::DragFloat("x##col_off_x", &colliderOff.x, 1.0f, -99999.0f, 99999.0f);
-				bool offActivatedX = ImGui::IsItemActivated();
-				ImGui::SameLine(0.0f, gap);
-				bool offChangedY = ImGui::DragFloat("y##col_off_y", &colliderOff.y, 1.0f, -99999.0f, 99999.0f);
-				bool offActivatedY = ImGui::IsItemActivated();
-				ImGui::PopItemWidth();
-
-				if (offActivatedX || offActivatedY) {
-					PushUndoSnapshot(editor, scene);
-				}
-
-				if (offChangedX || offChangedY) {
-					obj->SetColliderOffset({ colliderOff.x, colliderOff.y });
-					SyncColliderDefaults(colliderSize, { colliderOff.x, colliderOff.y });
-					scene.RebuildColliders();
-				}
+				ImGui::Columns(1);
 			}
-			else {
-				ImGui::TextDisabled("Collider disabled");
-			}
-
-			ImGui::NextColumn();
-
-			ImGui::Columns(1);
-
 			// ========== Audio Bindings Section ==========
 			ImGui::Spacing();
 			if (ImGui::CollapsingHeader("Audio Bindings")) {
