6f1538627eb7de91b61192a799ffe85f2d6f2599
chuuniby <takanashirikka501@gmail.com>
2026-03-13 14:31:02 +0800

    wrong path for one of the order

--- MyoonchiDiner/Core/CustomerOrderUILogic.cpp
+++ MyoonchiDiner/Core/CustomerOrderUILogic.cpp
@@ -327,7 +327,7 @@ const char* CustomerOrderUILogic::DishToIconPath(DishType dish) const {
 	case DishType::VegDish:  return "../assets/Salad.png";
 	case DishType::MeatDish: return "../assets/Meat.png";
 	case DishType::SoupDish: return "../assets/Soup.png";
-	case DishType::SkewerDish: return "../assets/Meat.png";
+	case DishType::SkewerDish: return "../assets/Food_Meat_n_carrot.png";
 	case DishType::CarrotSaladDish: return "../assets/Food_Salad_n_carrot.png";
 	case DishType::PoopDish:
 	default: return "../assets/PoopDish.png";
