8d4d7535b8db50cab02eb2085534152d1dc8ccec
CookieWasTaken <juinherng.ng@digipen.edu>
2026-03-13 12:32:30 +0800

    forgot to save

--- MyoonchiDiner/MyoonchiDinerBindings.cpp
+++ MyoonchiDiner/MyoonchiDinerBindings.cpp
@@ -1421,6 +1421,24 @@ else if (step == TutorialStep::CombineDishOnPlate) {
 			{ "btn_quit", [](Scene& scene, int id) {
 				scene.GetLogicManager().AddLogic<PauseButtonLogic>(id, PauseAction::Quit);
 			}},
+			{ "btn_next_level", [](Scene& scene, int id) {
+				auto* logic = scene.GetLogicManager().AddLogic<MenuButtonLogic>(id, FilePaths::Levels::KITCHEN_02, true);
+				if (logic && scene.GetAudioManager()) {
+					logic->SetAudioManager(scene.GetAudioManager());
+				}
+			}},
+			{ "btn_retry_level", [](Scene& scene, int id) {
+				auto* logic = scene.GetLogicManager().AddLogic<MenuButtonLogic>(id, scene.GetCurrentLevelPath(), true);
+				if (logic && scene.GetAudioManager()) {
+					logic->SetAudioManager(scene.GetAudioManager());
+				}
+			}},
+			{ "btn_main_menu", [](Scene& scene, int id) {
+				auto* logic = scene.GetLogicManager().AddLogic<MenuButtonLogic>(id, FilePaths::Levels::MAIN_MENU, false);
+				if (logic && scene.GetAudioManager()) {
+					logic->SetAudioManager(scene.GetAudioManager());
+				}
+			}},
 		};
 		return table;
 	}
